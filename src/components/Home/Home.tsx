6702826286f75ceabbc39633d12e50fdb24cb497
Danylo Melnychuk <dmelnychuk@exoft.net>
2023-02-13 14:45:37 +0200

    Add test redux

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,9 +1,16 @@
-import { FC } from "react";
-
+import { FC, useEffect } from "react";
+import { useLeagueDispatch, useLeagueSelector } from "../../hooks/redux";
+import { getLeagues } from "../../store/reducers/ActionCreators";
+  
 export const Home: FC = () => {
+  const dispatch = useLeagueDispatch();
+  const { leagues } = useLeagueSelector((state) => state.leagueReducer);
+
+  useEffect(() => dispatch(getLeagues()), []);
+
   return (
     <>
-      <h1>Hello</h1>
+      <h1>{JSON.stringify(leagues, null, 2)}</h1>
     </>
   );
 };
